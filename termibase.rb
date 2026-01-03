d0056e19c43f3c8f31b66939db597aa1906c5e25
tejgokani <tejmgokani@gmail.com>
2026-01-03 23:17:52 +0530

    Update SHA256 for v0.2.0 (includes 200 challenges)

--- termibase.rb
+++ termibase.rb
@@ -3,7 +3,7 @@ class Termibase < Formula
   desc "A terminal-native database learning playground"
   homepage "https://github.com/tejgokani/TermiBase"
   url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "c131a550e7222ccf633f1097a707e14763ffce3447eb09a5097af5874c11d294"
+  sha256 "0dad605992d78f1319948f27d1e01307a3945e32574b36856164931f04841a13"
   version "0.2.0"
   license "MIT"
 
