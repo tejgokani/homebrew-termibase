ff60a9768b9e05f6770c3fec74f5cbb74c6939f5
tejgokani <tejmgokani@gmail.com>
2026-01-03 22:42:18 +0530

    Update termibase to v0.2.0

--- termibase.rb
+++ termibase.rb
@@ -2,9 +2,9 @@
 class Termibase < Formula
   desc "A terminal-native database learning playground"
   homepage "https://github.com/tejgokani/TermiBase"
-  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "7f3ed74b53917eb034b8d625027b72a184499db628dee5e2308506607b6ddd4a"
-  version "0.1.7"
+  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "42ec0c57484bc1aa947b92866bdc6ef7a35efa02211ba293386d33e150df848f"
+  version "0.2.0"
   license "MIT"
 
   depends_on "python@3.11"
