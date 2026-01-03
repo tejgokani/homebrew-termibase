0e153bd1be006bdaeb28309334c677a7a10c3b90
tejgokani <tejmgokani@gmail.com>
2026-01-03 23:32:02 +0530

    Update termibase to v0.2.1 (add .sol command)

--- termibase.rb
+++ termibase.rb
@@ -2,9 +2,9 @@
 class Termibase < Formula
   desc "A terminal-native database learning playground"
   homepage "https://github.com/tejgokani/TermiBase"
-  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0dad605992d78f1319948f27d1e01307a3945e32574b36856164931f04841a13"
-  version "0.2.0"
+  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "e0a38074078261ad3111b9f1fa5c7cb58c285b2a5ea4465346641c22e42bfa99"
+  version "0.2.1"
   license "MIT"
 
   depends_on "python@3.11"
