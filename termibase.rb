037f8ca85315524b5afcdd30c3e04c11a927da78
tejgokani <tejmgokani@gmail.com>
2026-01-03 23:41:09 +0530

    Update termibase to v0.2.2 (fix evaluator validation)

--- termibase.rb
+++ termibase.rb
@@ -2,9 +2,9 @@
 class Termibase < Formula
   desc "A terminal-native database learning playground"
   homepage "https://github.com/tejgokani/TermiBase"
-  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "e0a38074078261ad3111b9f1fa5c7cb58c285b2a5ea4465346641c22e42bfa99"
-  version "0.2.1"
+  url "https://github.com/tejgokani/TermiBase/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "2bf83e5e9a2139b4fbe36690e0ed28997ae6725ec253774a7f6192a29027a0e2"
+  version "0.2.2"
   license "MIT"
 
   depends_on "python@3.11"
